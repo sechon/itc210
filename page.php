679d35af0258ee08da09b3c024db854da5e39614
Kristie Barker <kbarke05>
2015-10-15 19:26:31 -0700

    adds page.php template

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<!-- START CONTENT -->
+<div id="content" class="page">
+    <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content('<p class="serif">More &raquo;</p>'); ?>
+    <?php endwhile; ?>
+    <?php endif; ?>
+
+</div>
+<!-- END CONTENT -->
+
+<?php get_footer(); ?>

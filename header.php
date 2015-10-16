93d9251cdbfc951580f79b87648e2c7c2a1eb9d7
Kristie Barker <kbarke05>
2015-10-15 20:09:38 -0700

    adds header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage itc210
+ * @since ?
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="syle.css">
+	<title><?php bloginfo("name") . wp_title( '|' ); ?></title>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'itc210' ); ?></a>
+
+	
+
+	<div id="content" class="site-content">

66cab57e290c2c2cea70c544018fe88fed6a41f3
sechon <sechon01@seattlecentral.edu>
2015-11-10 19:55:00 -0800

    Connected Social

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
     <div class="col-md-4"> 
         <div id="social">
         <ul>
-        <li><a href="javascript:;" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/facebook.svg" alt="Facebook"></a></li>
-        <li><a href="javascript:;" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/twitter.svg" alt="Twitter"></a></li>
-        <li><a href="javascript:;" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/youtube.svg" alt="YouTube"></a></li>
-        <li><a href="javascript:;" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/pinterest.svg" alt="Pinterest"></a></li>
+        <li><a href="https://www.facebook.com/taynval" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/facebook.svg" alt="Facebook"></a></li>
+        <li><a href="https://www.twitter.com/taynval" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/twitter.svg" alt="Twitter"></a></li>
+        <li><a href="https://www.youtube.com/channel/UCibiDo4HiLXAcaalJsM5SAQ?sub_confirmation=1" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/youtube.svg" alt="YouTube"></a></li>
+        <li><a href="https://www.pinterest.com/taynval/" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/pinterest.svg" alt="Pinterest"></a></li>
         </ul>
         </div><!-- End Social -->
     </div>

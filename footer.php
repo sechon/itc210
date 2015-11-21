f078038480580aa83769f14058645ca84f160855
sechon <sechon01@seattlecentral.edu>
2015-11-21 13:34:40 -0800

    footer to match visual
    styled dreams div a bit, and matched footer to visual design

--- footer.php
+++ footer.php
@@ -1,45 +1,51 @@
      <hr>
 
     </div> <!-- /container -->
-<div class="rows">
-    <div class="col-md-4"> 
-        <div id="social">
+
+<div class="subscribe">
+    <div class="row">
+        <div class="col-md-6">
+        <p>If you'd like to be the first to know when we have swesome events going on in your city, do sign up for updates!</p>
+        </div>
+         <div class="col-md-6">
+        <p><input type="name" name="NAME" placeholder="Name" required />
+        <input type="email" name="EMAIL" placeholder="Your email address" required />
+        <input type="submit" class="btn btn-default btn-md" value="SUBSCRIBE" />
+        </p>
+        </div>
+    </div>
+</div>
+
+<div class="donate">
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Support Dreams Unlimited</h3>
+            <p>Fractured Atlas is a 501(c)(3) public charity. Contributions for the pirposes of Dreams Unlimited are tax-deductable to the extent permitted by law.</p>
+        </div>    
+        <div class="col-md-6">
+            <h3>About Fiscal Sponsorship</h3>
+            <p>Fractured Atlas's fiscal sponsorship program helps artists and arts organizations raise money from charitable sources.</p>
+        </div>
+    </div>
+    <p class="text-center"><a href="#"><button type="button" class="btn btn-default btn-lg">DONATE</button></a></p>
+</div>
+
+<div id="social" class="text-center"> 
         <ul>
         <li><a href="https://www.facebook.com/taynval" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/facebook.svg" alt="Facebook"></a></li>
         <li><a href="https://www.twitter.com/taynval" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/twitter.svg" alt="Twitter"></a></li>
         <li><a href="https://www.youtube.com/channel/UCibiDo4HiLXAcaalJsM5SAQ?sub_confirmation=1" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/youtube.svg" alt="YouTube"></a></li>
         <li><a href="https://www.pinterest.com/taynval/" target="_blank"><img class="social-logo" src="http://52.11.250.158/wp-content/uploads/2015/11/pinterest.svg" alt="Pinterest"></a></li>
-        </ul>
-        </div><!-- End Social -->
+        </ul>       
+</div><!-- End Social -->
+
+
+<footer>
+    <div class="footer">
+        <p class="text-center">© Dreams Unlimited <?php echo date('Y'); ?> | <a href="#">Privacy And Terms of Agreement</a></p>
     </div>
-    <div class="col-md-4">
-        <p>
-            <a href="#"><button type="button" class="btn btn-default btn-lg">DONATE</button></a>
-        </p>
-        </div>
-    <div class="col-md-4">
-        <p>
-        <button type="button" class="btn btn-default btn-lg">NEWSLETTER</button>
-        </p><p>
-        <input type="name" name="NAME" placeholder="Name" required />
-        </p><p>
-        <input type="email" name="EMAIL" placeholder="Your email address" required />
-        </p>
-        <p>
-        <input type="submit" value="SUBSCRIBE" />
-        </p>
-    </div>
-</div>
-<div class="rows">
-      <footer>
-        <div class="col-md-4">  
-        <p>© Dreams Unlimited 2015</p>
-        </div><div class="col-md-4">
-        <p>Privacy And Terms of Agreement</p>
-        </div>
-      </footer>
-</div>
-    <?php wp_footer(); ?>
+</footer>
+<?php wp_footer(); ?>
 
   </body>
 </html>
